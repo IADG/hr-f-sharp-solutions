603721fd9f18700703a25808403ea08930a33643
Răzvan Mocanu <razvan.mocanu@gmail.com>
2016-08-28 10:17:36 +0200

    ADD solution to exercise 3-4.

---
+++ Exercise 3-4.fsx
@@ -0,0 +1,35 @@
+﻿(*
+    Exercise 3-4 from Functional Programming Using F#
+    Copyright (c) 2016 Răzvan Mocanu
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
+
+type StraightLine = float * float
+
+let mirrorAroundXAxis (line: StraightLine) =
+    let m, b = line
+    ( -m, -b )
+
+let mirrorAroundYAxis (line: StraightLine) =
+    let m, b = line
+    ( -m, b )
+
+let printEquation (line: StraightLine) =
+    let m, b = line
+    "y = "
+  + string m
+  + "x"
+  + (if b < 0. then " - " else " + ")
+  + string (abs b)
