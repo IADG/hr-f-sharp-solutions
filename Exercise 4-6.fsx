204646630e15d768238ce0820150388873f4dcc2
Răzvan Mocanu <razvan.mocanu@gmail.com>
2016-09-04 14:10:01 +0200

    ADD solution to exercise 4-6.

---
+++ Exercise 4-6.fsx
@@ -0,0 +1,24 @@
+﻿(*
+    Exercise 4-6 from Functional Programming Using F#
+    Copyright (c) 2016 Răzvan Mocanu
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
+
+let isEven x = x % 2 = 0
+
+let rec filterEven list =
+    match list with
+    | x :: xs -> (if isEven x then [x] else []) @ filterEven xs
+    | _ -> []
