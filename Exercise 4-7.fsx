017321810b0f861993a52cd356f1c4462be23f87
Răzvan Mocanu <razvan.mocanu@gmail.com>
2016-09-04 18:45:54 +0200

    ADD solution for exercise 4-7.

---
+++ Exercise 4-7.fsx
@@ -0,0 +1,24 @@
+﻿(*
+    Exercise 4-7 from Functional Programming Using F#
+    Copyright (c) 2016 Răzvan Mocanu
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
+
+let rec calculateMultiplicity n xs =
+    match xs with
+    | x :: xs ->
+        let m = if n = x then 1 else 0
+        m + calculateMultiplicity n xs
+    | _ -> 0
