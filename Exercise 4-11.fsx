4d0e4af89bda7abd2cfa0751d0541bc0dce9fb54
Răzvan Mocanu <razvan.mocanu@gmail.com>
2016-09-09 21:50:18 +0200

    ADD solution for exercise 4-11.

---
+++ Exercise 4-11.fsx
@@ -0,0 +1,23 @@
+﻿(*
+    Exercise 4-11 from Functional Programming Using F#
+    Copyright (c) 2016 Răzvan Mocanu
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*)
+
+let rec count (xs, x) =
+    match xs with
+    | y :: xs when y < x -> count (xs, x)
+    | y :: xs when y = x -> 1 + count (xs, x)
+    | _ -> 0
